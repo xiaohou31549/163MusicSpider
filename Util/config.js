6b8d5c178e73024789b963f8a722bb9148459b20
li.jun <jun.li@reacheyes.com>
2015-09-24 11:49:30 +0800

    igonre product

--- Util/config.js
+++ Util/config.js
@@ -5,9 +5,9 @@
 exports.config = {
   baseUrl: 'http://music.163.com/song?id=',
   commentUrl: 'http://music.163.com/weapi/v1/resource/comments/R_SO_4_',
-  startSongId: 254105,
+  startSongId: 743302,
   endSongId: 999999999,
-  songMaxCount: 10,
-  minComments: 50,
+  songMaxCount: 110,
+  minComments: 800,
   playlistName: "网易爬虫第四波"
 };
